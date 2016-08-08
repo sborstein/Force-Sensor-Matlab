c813daddfbb8b6424a8736545e2831d900181cd4
Sam Borstein <sam@borstein.com>
2016-08-07 22:00:31 -0700

    updeted script

--- Gobiesox_Script.m
+++ Gobiesox_Script.m
@@ -1,19 +1,22 @@
 devices = daq.getDevices%get the devices
-devices(1)%see devices
-sesh = daq.createSession('ni');
+devices(1)%see devices, cDAQ-9171 should appear
+s = daq.createSession('ni');%create a session with NI-DAQ
 addAnalogInputChannel(s,'cDAQ1Mod1', 0, 'Voltage');%set channel 1 x
 addAnalogInputChannel(s,'cDAQ1Mod1', 1, 'Voltage');%set channel 2 y
 addAnalogInputChannel(s,'cDAQ1Mod1', 2, 'Voltage');%set channel 3 z%
 %ch.Range = [-2.5,2.5];%%need this for z, x/y are -5,5. Doesn't seem to be setting properly though
 
-s.Rate=2000; %%%Set the sampling rate. Don't get exactly why it is constrained/changed to certain values
-s.DurationInSeconds = 5;%Probably don't want this but rather a trigger?
-sesh
+s.Rate=1652; %%%Set the sampling rate. Don't get exactly why it is constrained/changed to certain values
+s.DurationInSeconds = 120;%Probably don't want this but rather a trigger?
+s
 %s.Channels(2).Sensitivity = 0.00922;not working, confused about error
-ch.ExcitationCurrent = .0020%%set excitation current for all channels to 2 mA
+%ch.ExcitationCurrent = .0020%%set excitation current for all channels to 2 mA
+
+%%addTriggerConnection(s,'External','cDAQMod1','StartTrigger');%Need to
+%%make a trigger probably.
 
 %%%RUN IT%%%
-[senseDat,timestamps] = startForeground(sesh);
+[senseDat,timestamps] = startForeground(s);
 figure
 plot(timestamps,senseDat)
 
